50c71129e7b5cc6e315face124e406c658b365ec
Todd Brunia <tbrunia@gmail.com>
2020-10-13 21:58:24 -0500

    chore: removed unnecessary function

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -1,31 +1,5 @@
 #!/bin/bash -ex
 
-wait_for_site_to_be_ready() {
-
-  siteUrl=${1}
-  count=0
-  sleepSeconds=5
-  maxAttempts=60
-
-  while [[ $(curl -s -o /dev/null -w '%{http_code}' "${siteUrl}") != "200" && "${count}" < "${maxAttempts}" ]]; do
-    sleep ${sleepSeconds}
-    count=$((count + 1))
-  done
-
-  if [[ "$count" == "${maxAttempts}" ]]; then
-    aws codebuild stop-build --id "${CODEBUILD_BUILD_ID}"
-  fi
-}
-
-execute_commands() {
-
-  commands=${1}
-
-  for command in ${commands}; do
-    echo "${command}" | base64 --decode | bash
-  done
-}
-
 create_or_update_cloudformation_stack() {
 
   pipelineId=${1}
