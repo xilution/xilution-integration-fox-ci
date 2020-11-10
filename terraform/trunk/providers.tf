cc32db8f56b6dd2c22731654d186f70ee8a3e61c
Todd Brunia <tbrunia@gmail.com>
2020-11-10 07:37:00 -0600

    chore: updated terraform aws provider version

--- terraform/trunk/providers.tf
+++ terraform/trunk/providers.tf
@@ -3,7 +3,7 @@ provider "aws" {
     role_arn = "arn:aws:iam::${var.client_aws_account}:role/xilution-developer-role"
   }
   region  = "us-east-1"
-  version = "2.41.0"
+  version = "3.14.1"
 }
 
 provider "null" {
