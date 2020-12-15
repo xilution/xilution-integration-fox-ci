1662c701d07e38b637108fd1b1eee749b01cfe9f
Todd Brunia <tbrunia@gmail.com>
2020-12-14 23:23:00 -0600

    fix: fixed export base api url

--- scripts/export-api-base-url.sh
+++ scripts/export-api-base-url.sh
@@ -3,10 +3,10 @@
 pipelineId=${FOX_PIPELINE_ID}
 stageName=${STAGE_NAME}
 stageNameLower=$(echo "${stageName}" | tr '[:upper:]' '[:lower:]')
-apiLambdaStackName="xilution-fox-${pipelineId}-stage-${stageNameLower}-api-lambda-stack"
+apiName="xilution-fox-${pipelineId:0:8}-${stageNameLower}-api"
 
-query=".Stacks[0].Outputs | map(select(.ExportName == \"${apiLambdaStackName}-api-base-url\")) | .[] .OutputValue"
-api_base_url=$(aws cloudformation describe-stacks --stack-name "${apiLambdaStackName}" | jq -r "${query}")
+query=".Items | map(select(.Name == \"${apiName}\")) | .[] .ApiEndpoint"
+api_base_url=$(aws apigatewayv2 get-apis | jq -r "${query}")
 
 export API_BASE_URL="${api_base_url}"
 
