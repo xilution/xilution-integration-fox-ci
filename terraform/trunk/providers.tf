48fee7ad20162526c985eb63b1e3e680d8c56200
Todd Brunia <tbrunia@gmail.com>
2021-01-30 16:11:14 -0600

    chore: fixed assume role

--- terraform/trunk/providers.tf
+++ terraform/trunk/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   assume_role {
-    role_arn = "arn:aws:iam::${var.client_aws_account}:role/xilution-developer-role"
+    role_arn = "arn:aws:iam::${var.client_aws_account}:role/xilution-agent-role"
   }
   region  = "us-east-1"
   version = "3.14.1"
