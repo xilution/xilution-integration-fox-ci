b9b6a03327405fe0b1fa41d60e02428e3567b83d
Todd Brunia <tbrunia@gmail.com>
2021-02-01 08:05:42 -0600

    fix: another attempt

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -99,7 +99,7 @@ resource "aws_apigatewayv2_authorizer" "authorizer" {
 # Public Routes
 
 resource "aws_apigatewayv2_route" "public_api_route" {
-  for_each  = toset(var.public_endpoints)
+  for_each  = toset({ for public_endpoint in var.public_endpoints : public_endpoint => public_endpoint })
   api_id    = aws_apigatewayv2_api.fox_api.id
   route_key = "${upper(each.value.method)} ${each.value.path}"
   target    = "integrations/${aws_apigatewayv2_integration.fox_api_integration.id}"
@@ -108,7 +108,7 @@ resource "aws_apigatewayv2_route" "public_api_route" {
 # Private Routes
 
 resource "aws_apigatewayv2_route" "private_api_route" {
-  for_each             = toset(var.private_endpoints)
+  for_each             = toset({ for private_endpoint in var.private_endpoints : private_endpoint => private_endpoint })
   api_id               = aws_apigatewayv2_api.fox_api.id
   route_key            = "${upper(each.value.method)} ${each.value.path}"
   target               = "integrations/${aws_apigatewayv2_integration.fox_api_integration.id}"
