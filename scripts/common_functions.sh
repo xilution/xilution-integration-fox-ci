eaa9ca88872e94b1e5480d8696f43e2dab7b90a1
Todd Brunia <tbrunia@gmail.com>
2021-02-21 09:34:46 -0600

    chore: added echo statements to wait for site to be ready

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -7,12 +7,16 @@ wait_for_site_to_be_ready() {
   sleepSeconds=5
   maxAttempts=60
 
+  echo "Checking the status of url: ${siteUrl}"
+
   while [[ $(curl -s -o /dev/null -w '%{http_code}' "${siteUrl}") != "200" && "${count}" -lt "${maxAttempts}" ]]; do
     sleep ${sleepSeconds}
     count=$((count + 1))
+    echo "Not ready yet."
   done
 
   if [[ "${count}" == "${maxAttempts}" ]]; then
+    echo "The site was never ready. Stopping the build now."
     aws codebuild stop-build --id "${CODEBUILD_BUILD_ID}"
   fi
 }
