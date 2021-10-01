420ad8393db46ef7eb221a6172cc003b2eef6ca7
Todd Brunia <tbrunia@gmail.com>
2021-10-01 08:42:01 -0500

    chore: fixed export terraform vars

--- scripts/export-terraform-vars.sh
+++ scripts/export-terraform-vars.sh
@@ -2,12 +2,16 @@
 
 [ -z "$1" ] && echo "The first argument should be the pipeline phase (trunk or stage)." && exit 1
 [ -z "$PIPELINE_TYPE" ] && echo "Didn't find PIPELINE_TYPE env var." && exit 1
+[ -z "$CODEBUILD_SRC_DIR_SourceCode" ] && echo "Didn't find CODEBUILD_SRC_DIR_SourceCode env var." && exit 1
 [ -z "$XILUTION_CONFIG" ] && echo "Didn't find XILUTION_CONFIG env var." && exit 1
 
 phase=${1}
 
-export ADD_INFRA_TERRAFORM_PATH_STAGE=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".additionalInfrastructure?.stage?.terraformModuleDir")
-export ADD_INFRA_TERRAFORM_PATH_TRUNK=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".additionalInfrastructure?.trunk?.terraformModuleDir")
+sourceDir=${CODEBUILD_SRC_DIR_SourceCode}
+addInfraTerraformPathStage=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".additionalInfrastructure?.stage?.terraformModuleDir")
+export ADD_INFRA_TERRAFORM_PATH_STAGE="${sourceDir}/${addInfraTerraformPathStage}"
+addInfraTerraformPathTrunk=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".additionalInfrastructure?.trunk?.terraformModuleDir")
+export ADD_INFRA_TERRAFORM_PATH_TRUNK="${sourceDir}/${addInfraTerraformPathTrunk}"
 
 if [[ "${PIPELINE_TYPE}" == "AWS_SMALL" ]]; then
   export TERRAFORM_PROVIDER="aws"
