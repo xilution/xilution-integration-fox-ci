6587286a9d9b0c1d1892797c35d3fa3d89fbf3f5
Todd Brunia <tbrunia@gmail.com>
2021-10-01 09:03:14 -0500

    chore: updated stage and trunk tfvars

--- scripts/build-aws-stage-terraform-vars.sh
+++ scripts/build-aws-stage-terraform-vars.sh
@@ -4,14 +4,15 @@
 [ -z "$XILUTION_ORGANIZATION_ID" ] && echo "Didn't find XILUTION_ORGANIZATION_ID env var." && exit 1
 [ -z "$GAZELLE_PIPELINE_ID" ] && echo "Didn't find GAZELLE_PIPELINE_ID env var." && exit 1
 [ -z "$FOX_PIPELINE_ID" ] && echo "Didn't find FOX_PIPELINE_ID env var." && exit 1
+[ -z "$PIPELINE_TYPE" ] && echo "Didn't find PIPELINE_TYPE env var." && exit 1
 [ -z "$CLIENT_AWS_ACCOUNT" ] && echo "Didn't find CLIENT_AWS_ACCOUNT env var." && exit 1
 [ -z "$CLIENT_AWS_REGION" ] && echo "Didn't find CLIENT_AWS_REGION env var." && exit 1
 [ -z "$XILUTION_AWS_ACCOUNT" ] && echo "Didn't find XILUTION_AWS_ACCOUNT env var." && exit 1
 [ -z "$XILUTION_AWS_REGION" ] && echo "Didn't find XILUTION_AWS_REGION env var." && exit 1
 [ -z "$XILUTION_ENVIRONMENT" ] && echo "Didn't find XILUTION_ENVIRONMENT env var." && exit 1
-[ -z "$PIPELINE_TYPE" ] && echo "Didn't find PIPELINE_TYPE env var." && exit 1
 [ -z "$STAGE_NAME" ] && echo "Didn't find STAGE_NAME env var." && exit 1
 [ -z "$COMMIT_ID" ] && echo "Didn't find COMMIT_ID env var." && exit 1
+[ -z "$DOMAIN" ] && echo "Didn't find DOMAIN env var." && exit 1
 
 handler=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".lambda.handler")
 runtime=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".lambda.runtime")
@@ -19,21 +20,41 @@ publicEndpoints=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".api.endp
 privateEndpoints=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".api.endpoints.private")
 jwtAuthorizer=$(echo "${XILUTION_CONFIG}" | base64 --decode | jq -r ".api.jwtAuthorizer")
 
+echo "XILUTION_ORGANIZATION_ID: ${XILUTION_ORGANIZATION_ID}"
+echo "GAZELLE_PIPELINE_ID: ${GAZELLE_PIPELINE_ID}"
+echo "FOX_PIPELINE_ID: ${FOX_PIPELINE_ID}"
+echo "PIPELINE_TYPE: ${PIPELINE_TYPE}"
+echo "CLIENT_AWS_ACCOUNT: ${CLIENT_AWS_ACCOUNT}"
+echo "CLIENT_AWS_REGION: ${CLIENT_AWS_REGION}"
+echo "XILUTION_AWS_ACCOUNT: ${XILUTION_AWS_ACCOUNT}"
+echo "XILUTION_AWS_REGION: ${XILUTION_AWS_REGION}"
+echo "XILUTION_ENVIRONMENT: ${XILUTION_ENVIRONMENT}"
+echo "STAGE_NAME: ${STAGE_NAME}"
+echo "COMMIT_ID: ${COMMIT_ID}"
+echo "DOMAIN: ${DOMAIN}"
+echo "handler: ${handler}"
+echo "runtime: ${runtime}"
+echo "publicEndpoints: ${publicEndpoints}"
+echo "privateEndpoints: ${privateEndpoints}"
+echo "jwtAuthorizer: ${jwtAuthorizer}"
+
 cat <<EOF >./tfvars.json
 {
   "organization_id": "$XILUTION_ORGANIZATION_ID",
   "gazelle_pipeline_id": "$GAZELLE_PIPELINE_ID",
   "fox_pipeline_id": "$FOX_PIPELINE_ID",
+  "pipeline_id": "$FOX_PIPELINE_ID",
+  "pipeline_type": "$PIPELINE_TYPE",
   "client_aws_account": "$CLIENT_AWS_ACCOUNT",
   "client_aws_region": "$CLIENT_AWS_REGION",
   "xilution_aws_account": "$XILUTION_AWS_ACCOUNT",
   "xilution_aws_region": "$XILUTION_AWS_REGION",
   "xilution_environment": "$XILUTION_ENVIRONMENT",
-  "pipeline_type": "$PIPELINE_TYPE",
   "stage_name": "$STAGE_NAME",
-  "lambda_runtime": "$runtime",
-  "lambda_handler": "$handler",
+  "domain": "$DOMAIN",
   "source_version": "$COMMIT_ID",
+  "lambda_handler": "$handler",
+  "lambda_runtime": "$runtime",
   "public_endpoints": $publicEndpoints,
   "private_endpoints": $privateEndpoints,
   "jwt_authorizer": $jwtAuthorizer
--- scripts/build-aws-trunk-terraform-vars.sh
+++ scripts/build-aws-trunk-terraform-vars.sh
@@ -3,23 +3,34 @@
 [ -z "$XILUTION_ORGANIZATION_ID" ] && echo "Didn't find XILUTION_ORGANIZATION_ID env var." && exit 1
 [ -z "$GAZELLE_PIPELINE_ID" ] && echo "Didn't find GAZELLE_PIPELINE_ID env var." && exit 1
 [ -z "$FOX_PIPELINE_ID" ] && echo "Didn't find FOX_PIPELINE_ID env var." && exit 1
+[ -z "$PIPELINE_TYPE" ] && echo "Didn't find PIPELINE_TYPE env var." && exit 1
 [ -z "$CLIENT_AWS_ACCOUNT" ] && echo "Didn't find CLIENT_AWS_ACCOUNT env var." && exit 1
 [ -z "$CLIENT_AWS_REGION" ] && echo "Didn't find CLIENT_AWS_REGION env var." && exit 1
 [ -z "$XILUTION_AWS_ACCOUNT" ] && echo "Didn't find XILUTION_AWS_ACCOUNT env var." && exit 1
 [ -z "$XILUTION_AWS_REGION" ] && echo "Didn't find XILUTION_AWS_REGION env var." && exit 1
 [ -z "$XILUTION_ENVIRONMENT" ] && echo "Didn't find XILUTION_ENVIRONMENT env var." && exit 1
-[ -z "$PIPELINE_TYPE" ] && echo "Didn't find PIPELINE_TYPE env var." && exit 1
+
+echo "XILUTION_ORGANIZATION_ID: ${XILUTION_ORGANIZATION_ID}"
+echo "GAZELLE_PIPELINE_ID: ${GAZELLE_PIPELINE_ID}"
+echo "FOX_PIPELINE_ID: ${FOX_PIPELINE_ID}"
+echo "PIPELINE_TYPE: ${PIPELINE_TYPE}"
+echo "CLIENT_AWS_ACCOUNT: ${CLIENT_AWS_ACCOUNT}"
+echo "CLIENT_AWS_REGION: ${CLIENT_AWS_REGION}"
+echo "XILUTION_AWS_ACCOUNT: ${XILUTION_AWS_ACCOUNT}"
+echo "XILUTION_AWS_REGION: ${XILUTION_AWS_REGION}"
+echo "XILUTION_ENVIRONMENT: ${XILUTION_ENVIRONMENT}"
 
 cat <<EOF >./tfvars.json
 {
   "organization_id": "$XILUTION_ORGANIZATION_ID",
   "gazelle_pipeline_id": "$GAZELLE_PIPELINE_ID",
   "fox_pipeline_id": "$FOX_PIPELINE_ID",
+  "pipeline_id": "$FOX_PIPELINE_ID",
+  "pipeline_type": "$PIPELINE_TYPE",
   "client_aws_account": "$CLIENT_AWS_ACCOUNT",
   "client_aws_region": "$CLIENT_AWS_REGION",
   "xilution_aws_account": "$XILUTION_AWS_ACCOUNT",
   "xilution_aws_region": "$XILUTION_AWS_REGION",
-  "xilution_environment": "$XILUTION_ENVIRONMENT",
-  "pipeline_type": "$PIPELINE_TYPE"
+  "xilution_environment": "$XILUTION_ENVIRONMENT"
 }
 EOF
